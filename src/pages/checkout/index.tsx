cc889e4c3a12400d07c9c14f3bf8386bb0842a09
Felipe Soares <felipe.11.11@hotmail.com>
2023-10-20 13:52:31 -0400

    Feat: Separate Zod Validation form

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -9,63 +9,14 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useNavigate } from 'react-router-dom'
 import { useContext } from 'react'
 import { ProductsContext } from '../../context/ProductsContext'
+import { formDeliverySchema } from './validation/zod'
+import { toast } from 'react-toastify'
 
-const RegexCepValidate = /(^\d{8}$)|(^\d{5}-\d{3}$)/
-const UFArray = [
-  'AC',
-  'AL',
-  'AP',
-  'AM',
-  'BA',
-  'CE',
-  'DF',
-  'ES',
-  'GO',
-  'MA',
-  'MS',
-  'MT',
-  'MG',
-  'PA',
-  'PB',
-  'PR',
-  'PE',
-  'PI',
-  'RJ',
-  'RN',
-  'RS',
-  'RO',
-  'RR',
-  'SC',
-  'SP',
-  'SE',
-  'TO',
-] as const
-
-const formSchema = z.object({
-  CEP: z.string().refine((val) => val.match(RegexCepValidate), {
-    message: 'Cep inválido, Ex:(12345-123 ou 12345678)',
-  }),
-  Street: z.string().min(1, 'O campo Rua é Obrigatório'),
-  Number: z.coerce.number().min(1, 'O número não pode ser negativo'),
-  Complement: z.string().optional(),
-  Neighborhood: z.string().min(1, 'O campo Bairro é Obrigatório'),
-  City: z.string().min(1, 'O campo Cidade é Obrigatório'),
-  UF: z
-    .string()
-    .min(2, 'O campo UF é Obrigatório')
-    .max(2, 'O campo UF é tem apenas 2 caracteres')
-    .transform((val) => val.toUpperCase())
-    .refine((val) => UFArray.includes(val), {
-      message: 'UF não encontrado',
-    }),
-  Payment: z.string().min(1, 'Escolha uma opção de pagamento'),
-})
-
-export type formDeliveryData = z.infer<typeof formSchema>
+export type formDeliveryData = z.infer<typeof formDeliverySchema>
 
 export function Checkout() {
   const DeliveryForm = useForm<formDeliveryData>({
-    resolver: zodResolver(formSchema),
+    resolver: zodResolver(formDeliverySchema),
   })
   const { handleSubmit } = DeliveryForm
   const navigate = useNavigate()
@@ -75,6 +26,7 @@ export function Checkout() {
     navigate('/checkout/success')
     addNewAdress(data)
     clearCart()
+    toast.success('Compra realizada ')
   }
   return (
     <form onSubmit={handleSubmit(handleNewOrder)}>
---
+++ src/pages/checkout/validation/zod.ts
@@ -0,0 +1,52 @@
+import { z } from 'zod'
+
+const RegexCepValidate = /(^\d{8}$)|(^\d{5}-\d{3}$)/
+const UFArray = [
+  'AC',
+  'AL',
+  'AP',
+  'AM',
+  'BA',
+  'CE',
+  'DF',
+  'ES',
+  'GO',
+  'MA',
+  'MS',
+  'MT',
+  'MG',
+  'PA',
+  'PB',
+  'PR',
+  'PE',
+  'PI',
+  'RJ',
+  'RN',
+  'RS',
+  'RO',
+  'RR',
+  'SC',
+  'SP',
+  'SE',
+  'TO',
+] as const
+
+export const formDeliverySchema = z.object({
+  CEP: z.string().refine((val) => val.match(RegexCepValidate), {
+    message: 'Cep inválido, Ex:(12345-123 ou 12345678)',
+  }),
+  Street: z.string().min(1, 'O campo Rua é Obrigatório'),
+  Number: z.coerce.number().min(1, 'O número não pode ser negativo'),
+  Complement: z.string().optional(),
+  Neighborhood: z.string().min(1, 'O campo Bairro é Obrigatório'),
+  City: z.string().min(1, 'O campo Cidade é Obrigatório'),
+  UF: z
+    .string()
+    .min(2, 'O campo UF é Obrigatório')
+    .max(2, 'O campo UF é tem apenas 2 caracteres')
+    .transform((val) => val.toUpperCase())
+    .refine((val) => UFArray.includes(val), {
+      message: 'UF não encontrado',
+    }),
+  Payment: z.string().min(1, 'Escolha uma opção de pagamento'),
+})
