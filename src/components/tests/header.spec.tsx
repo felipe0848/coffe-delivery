03602542d834a6456f9715c6eb9a676b8ed4c3f2
Felipe Soares <felipe.11.11@hotmail.com>
2023-10-30 08:20:13 -0400

    Feat: Add test navigate when hits a NavLink

--- src/components/tests/header.spec.tsx
+++ src/components/tests/header.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { Header } from '../header'
 import '@testing-library/jest-dom'
 import { BrowserRouter } from 'react-router-dom'
@@ -9,19 +9,45 @@ jest.mock('react-router', () => ({
   ...jest.requireActual('react-router'),
   useNavigate: () => mockNavigate,
 }))
-
+const RenderComponent = () => {
+  render(
+    <ProductsContextProvider>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </ProductsContextProvider>,
+  )
+}
 describe('Header', () => {
   it('should render correctly', () => {
-    render(
-      <ProductsContextProvider>
-        <BrowserRouter>
-          <Header />
-        </BrowserRouter>
-      </ProductsContextProvider>,
-    )
+    RenderComponent()
 
     expect(screen.getByTestId('logo-anchor')).toBeInTheDocument()
     expect(screen.getByText('Campo Grande, MS')).toBeInTheDocument()
     expect(screen.getByTitle('Ir para o carrinho')).toBeInTheDocument()
+  })
+  it('should call navigate when hits the anchor logo', () => {
+    RenderComponent()
+    const logoAnchor = screen.getByTestId('logo-anchor')
+
+    fireEvent.click(logoAnchor)
+    expect(mockNavigate).toHaveBeenCalledWith('/', {
+      preventScrollReset: undefined,
+      relative: undefined,
+      replace: true,
+      state: undefined,
+    })
+  })
+  it('should call navigate when hits the button cart', () => {
+    RenderComponent()
+    const btnCart = screen.getByTitle('Ir para o carrinho')
+
+    fireEvent.click(btnCart)
+    expect(mockNavigate).toHaveBeenCalledWith('/checkout', {
+      preventScrollReset: undefined,
+      relative: undefined,
+      replace: false,
+      state: undefined,
+    })
   })
 })
